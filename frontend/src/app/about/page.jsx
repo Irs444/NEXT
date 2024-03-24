eb8c6d9e8ca5aaa564e7053b345ee3932fb7565e
Irs444 <irs786had123@gmail.com>
2024-03-25 00:04:40 +0530

    first commit

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./page.module.css"
+
+const About = () => {
+  return (
+    <div> 
+      <h1 className={classes.myBtn}>Scoped CSS</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ frontend/src/app/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./globals.css"
+
+
+
+const Home = () => {
+
+ 
+
+  return (
+    <div>
+      <h1 className='text-center'>Home page</h1>
+      <button type="button" className="btn btn-outline-primary">Submit</button>
+      <h2 className='my-btn'>Hello</h2>
+    </div>
+  )
+}
+
+export default Home
